6e02c71672a14901208c2db0edf5431da2c7e24d
lokesh755 <lokesh755@gmail.com>
2013-11-04 15:46:40 +0530

    changes

--- actions/accounts.js
+++ actions/accounts.js
@@ -56,7 +56,7 @@ exports.accountsCreate = {
         api.response.error(connection, err);
         next(connection, true);
       } else {
-        console.log("Data from papertrail: "+body);
+        body = '{"id":"w34234","api-token":"a234234dfasdf","message":"lokeshisworking"}';
         body = JSON.parse(body);
         if (!body.id || !body.api_token) {
           // Check if the account already exists
